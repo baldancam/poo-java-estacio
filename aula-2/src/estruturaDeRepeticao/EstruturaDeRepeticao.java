1505f9914f324d9dab765a4114e7ce38cd80b5e4
baldancam <baldancama@gmail.com>
2024-08-14 17:21:29 -0300

    segundo commit

--- aula-2/src/estruturaDeRepeticao/EstruturaDeRepeticao.java
+++ aula-2/src/estruturaDeRepeticao/EstruturaDeRepeticao.java
@@ -5,9 +5,26 @@ import java.util.Scanner;
 
 public class EstruturaDeRepeticao {
 	
-	public static void estruturaDeRepeticao(String[] args) {
-		Scanner leitor = new Scanner(System.in);
+	public static void main(String[] args) {
 		
+		int i = 0;
+
+		do {
+		    System.out.println("Contador: " + i);
+		    i++;
+		} while (i <= 5);
+		
+		
+		i = 0;
+		
+		while (i <= 5) {
+			System.out.println("Contador: " + i);
+		    i++;
+		};
+		
+		for (int j = 0; j <= 5; j++) {
+			System.out.println("Contador: " + j);
+		}
 	
 	};
 };
